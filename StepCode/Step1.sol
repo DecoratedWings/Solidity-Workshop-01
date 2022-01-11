21bd286c647843fdee87719f85ea1a7768d13523
Mona Rassouli <mona@Monas-MacBook-Pro.local>
2022-01-11 10:56:41 -0500

    Step 1 updated

---
+++ StepCode/Step1.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.3; 
+
+//import 
+
+contract Hello {
+     string public str = "Hello World!";
+}
+
+//Speaker Notes:
+//========
+//Intro to Remix
+//Compile, deploy, and gas costs 
+//Environment connection
+//Interaction with deployed contract
+//public keyword automatically creates getter
